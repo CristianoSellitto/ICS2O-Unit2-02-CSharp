0169bc84e1bd2f5a4a291d0ffea85479d98f7a85
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-11 17:44:02 +0000

    Added math

--- main.cs
+++ main.cs
@@ -1,7 +1,16 @@
+// Cristiano | ICS2O-Unit2-02-CSharp | March 11 2022
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("3 + 6 = " + (3 + 6));
+        Console.WriteLine("3 - 6 = " + (3 - 6));
+        Console.WriteLine("3 × 6 = " + (3 * 6));
+        Console.WriteLine("6 ÷ 3 = " + (6 / 3));
+        Console.WriteLine("3 + 6² = " + (3 + Math.Pow(6, 2)));
+        Console.WriteLine("\nFinished.");
+    }
 }
